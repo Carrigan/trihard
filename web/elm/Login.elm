dce87d58c52d385e229452f711480d4a1588d3f4
Brian Carrigan <brian.carrigan@smashingboxes.com>
2016-02-03 23:27:47 -0500

    Moved more of the login modal to the Login elm module.

--- web/elm/Login.elm
+++ web/elm/Login.elm
@@ -1,6 +1,25 @@
 module Login where
 
-import Html
+import Html exposing (div, button, text, input, label)
+import Html.Attributes exposing (attribute, id, type')
 
 main =
-  Html.text "Hello from Elm"
+  div []
+  [ div [ attribute "class" "modal-body" ]
+      [ label [] [text "Email or Username"]
+      , input
+          [ attribute "class" "form-control"
+          , id "username"
+          ]
+          []
+      , label [] [text "Password"]
+      , input
+          [ attribute "class" "form-control"
+          , type' "password"
+          , id "password"
+          ]
+          []
+      ]
+  , div [ attribute "class" "modal-footer" ]
+      [ button [attribute "class" "btn btn-primary" ] [ Html.text "Login" ] ]
+  ]
